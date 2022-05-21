d71a0ba22144356dd0c3a88d239ae209f13e4b93
Ben Ritter <benjaminaritter@gmail.com>
2022-05-21 21:59:39 +0800

    rm commented code

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,3 @@ mod road_parts;
 mod road_functions;
 
 mod network;
-
-// trait AreaMarking {
-//     fn hasBorder(&self) -> bool;
-//     fn hasFill(&self) -> bool;
-// }
-
-// impl CrossSection for CorridorElement { }
