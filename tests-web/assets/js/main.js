319f000842800fe51f2ac6e7496e7be53d929650
Dustin Carlino <dabreegster@gmail.com>
2022-07-23 16:32:45 +0200

    Format the modified files with prettier

--- tests-web/assets/js/main.js
+++ tests-web/assets/js/main.js
@@ -2,31 +2,30 @@ import { makeDropHandler, makeLinkHandler, handleDragOver } from "./files.js";
 import { makeOpenTest, loadTests } from "./tests.js";
 
 const useMap = (map) => {
-    const container = map.getContainer();
-    container.ondrop = makeDropHandler(map);
-    container.ondragover = handleDragOver;
+  const container = map.getContainer();
+  container.ondrop = makeDropHandler(map);
+  container.ondragover = handleDragOver;
 
-    map.loadLink = makeLinkHandler(map);
-    map.openTest = makeOpenTest(map)
-    console.info("New map created! File drops enabled.", container);
+  map.loadLink = makeLinkHandler(map);
+  map.openTest = makeOpenTest(map);
+  console.info("New map created! File drops enabled.", container);
 
+  // Here we read the test name from the URL.
+  const q = new URLSearchParams(window.location.search);
+  if (q.has("test")) {
+    const test = q.get("test");
+    console.info("Loading test " + test + " from URL.");
+    map.openTest(test);
+  }
 
-    // Here we read the test name from the URL.
-    const q = new URLSearchParams(window.location.search);
-    if (q.has('test')) {
-        const test = q.get('test');
-        console.info("Loading test " + test + " from URL.");
-        map.openTest(test);
-    }
-
-    loadTests();
-}
+  loadTests();
+};
 
 // Initialize the map
-const map = L.map('map', { maxZoom: 21 }).setView([40.0, 10.0], 4);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxNativeZoom: 18,
-    maxZoom: 21,
-    attribution: '© OpenStreetMap'
+const map = L.map("map", { maxZoom: 21 }).setView([40.0, 10.0], 4);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxNativeZoom: 18,
+  maxZoom: 21,
+  attribution: "© OpenStreetMap",
 }).addTo(map);
 useMap(map);
