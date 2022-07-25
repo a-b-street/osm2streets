6ba64d824ac7f914aa390c609f681b80c5fd2297
Dustin Carlino <dabreegster@gmail.com>
2022-07-25 09:29:05 +0100

    Move the import logic to its own .js

--- tests-web/assets/js/main.js
+++ tests-web/assets/js/main.js
@@ -1,9 +1,6 @@
 import { makeDropHandler, makeLinkHandler, handleDragOver } from "./files.js";
+import { makeImportCurrentView } from "./import.js";
 import { makeOpenTest, loadTests } from "./tests.js";
-
-// TODO The hash changes every time the Rust code does, this is very brittle.
-// See https://github.com/thedodd/trunk/issues/230 or stop using trunk.
-import { import_osm } from "../osm2streets-js-5b7f56a6bec77166.js";
 
 const useMap = (map) => {
   const container = map.getContainer();
@@ -23,6 +20,8 @@ const useMap = (map) => {
   }
 
   loadTests();
+
+  makeImportCurrentView(map, document.getElementById("import-view"));
 };
 
 // Initialize the map
@@ -33,44 +32,3 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "© OpenStreetMap",
 }).addTo(map);
 useMap(map);
-
-// TODO Should we split this into another .js file? How should it get the map if so?
-// TODO Is it OK to just assume the button exists by the time this script runs,
-// or should we create the button here?
-const btn = document.getElementById("import-view");
-btn.onclick = async function importCurrentView() {
-  if (map.getZoom() < 15) {
-    window.alert("Zoom in more to import");
-  }
-
-  // Grab OSM XML from Overpass
-  // (Sadly toBBoxString doesn't seem to match the order for Overpass)
-  const b = map.getBounds();
-  const bbox = `${b.getSouth()},${b.getWest()},${b.getNorth()},${b.getEast()}`;
-  const query = `(nwr(${bbox}); node(w)->.x; <;); out meta;`;
-  const url = `https://overpass-api.de/api/interpreter?data=${query}`;
-  console.log(`Fetching from overpass: ${url}`);
-
-  btn.innerText = "Downloading from Overpass...";
-  // Prevent this function from happening twice in a row. It could also
-  // maybe be nice to allow cancellation.
-  btn.disabled = true;
-
-  const resp = await fetch(url);
-  // TODO Error handling and such
-  const osmXML = await resp.text();
-
-  btn.innerText = "Importing OSM data...";
-
-  const output = import_osm(osmXML, {
-    // TODO Ask overpass
-    driving_side: "Right",
-  });
-
-  // TODO Definitely time to think about cleaning up old layers
-  L.geoJSON(JSON.parse(output), { style: { color: "#f55" } }).addTo(map);
-
-  // Make the button clickable again
-  btn.innerText = "Import current view";
-  btn.disabled = false;
-};
