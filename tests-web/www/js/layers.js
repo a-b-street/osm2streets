8bdb8a4f828d73286509d47f171c316dc71e5b06
Ben Ritter <benjaminaritter@gmail.com>
2022-07-28 15:28:38 +0800

    Colour road areas based on intersection complexity

--- tests-web/www/js/layers.js
+++ tests-web/www/js/layers.js
@@ -1,6 +1,27 @@
 export const makeJsonLayer = (text) => {
-    return new L.geoJSON(JSON.parse(text), { style: { color: '#f55' }});
+    return new L.geoJSON(JSON.parse(text), { style: styleGeoJson });
 };
+
+const intersectionColours = {
+    MapEdge: '#696',
+    Connection: '#666',
+    MultiConnection: '#669',
+    Merge: '#969',
+    Crossing: '#999',
+    Terminus: '#966',
+}
+
+const styleGeoJson = (feature) => {
+    if (feature.geometry.type === 'Polygon') {
+        console.log(feature.properties?.complexity);
+        return {
+            color: intersectionColours[feature.properties?.complexity] || '#666',
+            weight: 1,
+            fillOpacity: 0.7,
+        };
+    }
+    return { color: '#f55' };
+}
 
 export const makeOsmLayer = (text) => {
     return new L.OSM.DataLayer(new DOMParser().parseFromString(text, 'application/xml'), { style: { color: '#5f5' }});
