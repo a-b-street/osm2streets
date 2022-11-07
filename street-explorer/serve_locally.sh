62d0883ff245fffdf30f4fc75c4b27d7773c2e4b
Ben Ritter <benjaminaritter@gmail.com>
2022-11-08 00:03:56 +0800

    Use `set -x` to print commands to provide more context

--- street-explorer/serve_locally.sh
+++ street-explorer/serve_locally.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+set -e # exit on failure
+set -x # print commands as they are run
 
-wasm-pack build --dev --target web ../osm2streets-js && python3 -m http.server --directory www/
+wasm-pack build --dev --target web ../osm2streets-js
+python3 -m http.server --directory www/
