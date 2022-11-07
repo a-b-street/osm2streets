23f604821874fdacac5e08cf28de653ddfa25e3d
Ben Ritter <benjaminaritter@gmail.com>
2022-11-08 00:17:56 +0800

    Use a unique port so this app exists on its own host
    Port 8111 is used by JOSM, so 8112 seems as good as any other.

--- street-explorer/serve_locally.sh
+++ street-explorer/serve_locally.sh
@@ -3,4 +3,4 @@ set -e # exit on failure
 set -x # print commands as they are run
 
 wasm-pack build --dev --target web ../osm2streets-js
-python3 -m http.server --directory www/
+python3 -m http.server --directory www/ 8112
