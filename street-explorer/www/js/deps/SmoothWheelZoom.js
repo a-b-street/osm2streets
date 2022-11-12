cea307e1513c1356af03cc5956c19023a602c7e2
Ben Ritter <benjaminaritter@gmail.com>
2022-11-12 14:58:22 +0800

    Fix scroll zoom jank when mouse is moving

--- street-explorer/www/js/deps/SmoothWheelZoom.js
+++ street-explorer/www/js/deps/SmoothWheelZoom.js
@@ -36,7 +36,7 @@ L.Map.SmoothWheelZoom = L.Handler.extend({
         this._wheelMousePosition = map.mouseEventToContainerPoint(e);
         this._centerPoint = map.getSize()._divideBy(2);
         this._startLatLng = map.containerPointToLatLng(this._centerPoint);
-        this._wheelStartLatLng = map.containerPointToLatLng(this._wheelMousePosition);
+        this._wheelMouseLatLng = map.containerPointToLatLng(this._wheelMousePosition);
         this._startZoom = map.getZoom();
         this._moved = false;
         this._zooming = true;
@@ -59,6 +59,7 @@ L.Map.SmoothWheelZoom = L.Handler.extend({
             this._goalZoom = map._limitZoom(this._goalZoom);
         }
         this._wheelMousePosition = this._map.mouseEventToContainerPoint(e);
+        this._wheelMouseLatLng = map.containerPointToLatLng(this._wheelMousePosition);
 
         clearTimeout(this._timeoutId);
         this._timeoutId = setTimeout(this._onWheelEnd.bind(this), 200);
@@ -89,7 +90,7 @@ L.Map.SmoothWheelZoom = L.Handler.extend({
         if (map.options.smoothWheelZoom === 'center') {
             this._center = this._startLatLng;
         } else {
-            this._center = map.unproject(map.project(this._wheelStartLatLng, this._zoom).subtract(delta), this._zoom);
+            this._center = map.unproject(map.project(this._wheelMouseLatLng, this._zoom).subtract(delta), this._zoom);
         }
 
         if (!this._moved) {
