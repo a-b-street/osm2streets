9515a8c48a0147bb3e481463e560daf278bdeb28
Dustin Carlino <dabreegster@gmail.com>
2022-12-30 12:08:03 +0000

    Fix up docs and CI. #162
    Turns out we also need a top-level await polyfill

--- street-explorer/vite.config.js
+++ street-explorer/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import wasmPack from "vite-plugin-wasm-pack";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
   build: {
@@ -12,5 +13,5 @@ export default defineConfig({
     },
   },
   base: "/osm2streets/",
-  plugins: [wasmPack(["../osm2streets-js"])],
+  plugins: [wasmPack(["../osm2streets-js"]), topLevelAwait()],
 });
