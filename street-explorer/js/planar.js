1ef458b6f3310e8ceae11dacff36826226523e66
Dustin Carlino <dabreegster@gmail.com>
2023-01-11 19:33:01 +0000

    Delete overlapping faces in the UI to kinda debug them
    there's a huge face covering everything. it's... correct, i think just
    the winding order is unexpected

--- street-explorer/js/planar.js
+++ street-explorer/js/planar.js
@@ -31,31 +31,30 @@ export function addPlanar(group, network, map) {
   );
 
   let faceJson = JSON.parse(network.toPlanarGeojsonFaces());
-  group.addLayer(
-    "Planar graph (faces)",
-    new L.geoJSON(faceJson, {
-      style: function (feature) {
-        return feature.properties;
-      },
-      onEachFeature: function (feature, layer) {
-        layer.on({
-          mouseover: function (ev) {
-            ev.target.setStyle({
-              fillOpacity: 0.1,
-            });
-          },
-          mouseout: function (ev) {
-            layer.setStyle({
-              fillOpacity: 0.5,
-            });
-          },
-        });
-      },
-    })
-  );
+  let faceLayer = new L.geoJSON(faceJson, {
+    style: function (feature) {
+      return feature.properties;
+    },
+    onEachFeature: function (feature, layer) {
+      layer.on({
+        mouseover: function (ev) {
+          ev.target.setStyle({
+            fillOpacity: 0.1,
+          });
+        },
+        mouseout: function (ev) {
+          layer.setStyle({
+            fillOpacity: 0.5,
+          });
+        },
+      });
+    },
+  });
+  group.addLayer("Planar graph (faces)", faceLayer);
 
   let div = document.getElementById("crowdedStuff");
   let cursorPos = document.getElementById("cursorPos");
+  let someFaceId = null;
   map.on({
     mousemove: (e) => {
       let pt = [e.latlng.lng, e.latlng.lat];
@@ -80,12 +79,46 @@ export function addPlanar(group, network, map) {
         div.innerHTML = `${points} nodes, ${lines} edges by cursor`;
       }
 
+      let faces = 0;
+      someFaceId = null;
       for (let feature of faceJson.features) {
         if (booleanPointInPolygon(pt, feature)) {
-          cursorPos.innerText = `Face: ${feature.properties.sources}`;
-          break;
+          faces++;
+          div.innerText = `Face: ${feature.properties.sources}`;
+          if (faces == 1) {
+            someFaceId = feature.properties.id;
+          }
         }
       }
+      if (faces > 1) {
+        div.innerHTML = `<h1 style="color: red">${faces} faces</h1>`;
+      }
     },
+  });
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key == "Delete") {
+      console.log(`Deleted ${someFaceId}`);
+      faceLayer.eachLayer((layer) => {
+        if (layer.feature.properties.id == someFaceId) {
+          faceLayer.removeLayer(layer);
+        }
+      });
+
+      faceJson.features = faceJson.features.filter(
+        (f) => f.properties.id != someFaceId
+      );
+    } else if (e.key == "Backspace") {
+      console.log(`Deleted everything except ${someFaceId}`);
+      faceLayer.eachLayer((layer) => {
+        if (layer.feature.properties.id != someFaceId) {
+          faceLayer.removeLayer(layer);
+        }
+      });
+
+      faceJson.features = faceJson.features.filter(
+        (f) => f.properties.id == someFaceId
+      );
+    }
   });
 }
