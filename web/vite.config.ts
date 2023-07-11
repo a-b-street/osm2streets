7135496afe5018de6b77544fa8c5acc3b8f9d58e
Dustin Carlino <dabreegster@gmail.com>
2023-07-11 10:10:15 +0100

    Fix vite config, incorrectly copied from another repo

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import wasmPack from "vite-plugin-wasm-pack";
 
 export default defineConfig({
-  base: "/osm2svelte/",
+  base: "/osm2streets/",
   build: {
     rollupOptions: {
       input: {
