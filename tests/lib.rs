7f5b8d34c5680dc6d1fcd67a1d28d1c9d964f470
dabreegster <dabreegster@users.noreply.github.com>
2023-12-06 13:51:49 +0000

    deploy: 21b8283ec4a35196677a9544c249eef006b6f69a

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,13 +1,14 @@
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
     use std::sync::Once;
 
+    use abstutil::Timer;
     use anyhow::{bail, Result};
     use env_logger::{Builder, Env};
-
-    use abstutil::Timer;
-    use experimental::RoadNetwork;
     use geom::LonLat;
+
+    use experimental::RoadNetwork;
     use osm2streets::{MapConfig, Transformation};
 
     static SETUP_LOGGER: Once = Once::new();
@@ -28,38 +29,52 @@ mod tests {
         let prior_dot = std::fs::read_to_string(format!("{path}/road_network.dot"))
             .unwrap_or_else(|_| String::new());
 
-        let clip_pts = Some(LonLat::read_geojson_polygon(&format!(
-            "{path}/boundary.json"
-        ))?);
-        let (mut street_network, _) = streets_reader::osm_to_street_network(
-            &std::fs::read_to_string(format!("{path}/input.osm"))?,
-            clip_pts,
-            MapConfig::default(),
-            &mut timer,
-        )?;
+        let clip_pts = if Path::new(format!("{path}/boundary.json").as_str()).exists() {
+            Some(LonLat::read_geojson_polygon(&format!(
+                "{path}/boundary.json"
+            ))?)
+        } else {
+            None
+        };
+
+        let (mut street_network, _) = if Path::new(format!("{path}/input.osm").as_str()).exists() {
+            streets_reader::osm_to_street_network(
+                &std::fs::read_to_string(format!("{path}/input.osm"))?,
+                clip_pts,
+                MapConfig::default(),
+                &mut timer,
+            )?
+        } else {
+            streets_reader::pbf_to_street_network(
+                &std::fs::read(format!("{path}/input.osm.pbf").as_str())?,
+                clip_pts,
+                MapConfig::default(),
+                &mut timer,
+            )?
+        };
         street_network.check_invariants();
         street_network.apply_transformations_with_invariant_checks(
             Transformation::standard_for_clipped_areas(),
             &mut timer,
         );
         street_network.save_to_geojson(format!("{path}/geometry.json"))?;
-
         let road_network: RoadNetwork = street_network.into();
+
         std::fs::write(format!("{path}/road_network.dot"), road_network.to_dot())?;
 
         let current_dot = std::fs::read_to_string(format!("{path}/road_network.dot"))?;
         if prior_dot != current_dot {
             std::fs::write(format!("{path}/road_network.orig.dot"), prior_dot)?;
-            bail!("./{}/road_network.dot is different! If it is OK, commit it.
-./{0}/road_network.orig.dot is previous result. Compare it on https://doctorbud.com/graphviz-viewer/", path);
+            bail!("./{}/road_network.dot is different! If it is OK, commit it. \
+            ./{0}/road_network.orig.dot is previous result. Compare it on https://doctorbud.com/graphviz-viewer/", path);
         }
 
         let current_json = std::fs::read_to_string(format!("{path}/geometry.json"))?;
         if prior_json != current_json {
             std::fs::write(format!("{path}/geometry.orig.json"), prior_json)?;
             bail!(
-                "./{}/geometry.json is different! If it is OK, commit it.
-./{0}/geometry.orig.json is previous result. Compare it on https://geojson.io",
+                "./{}/geometry.json is different! If it is OK, commit it. \
+                ./{0}/geometry.orig.json is previous result. Compare it on https://geojson.io",
                 path
             );
         }
