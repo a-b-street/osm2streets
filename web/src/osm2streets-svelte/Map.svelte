4585f38f1cf548c7e623bbfd54dd47757f0bb07d
Dustin Carlino <dabreegster@gmail.com>
2024-03-14 17:33:02 +0000

    Introduce svelte-maplibre. Just swap out the first piece, with everything else working the same

--- web/src/osm2streets-svelte/Map.svelte
+++ web/src/osm2streets-svelte/Map.svelte
@@ -1,49 +1,38 @@
 <script lang="ts">
-  import { Map, NavigationControl, ScaleControl } from "maplibre-gl";
-  import { onDestroy, onMount } from "svelte";
-  import "maplibre-gl/dist/maplibre-gl.css";
+  import { MapLibre, ScaleControl, NavigationControl } from "svelte-maplibre";
+  import type { Map } from "maplibre-gl";
   import { map as mapStore } from "./store";
 
   let map: Map;
-  let mapContainer: HTMLDivElement;
   let loaded = false;
 
-  onMount(() => {
-    map = new Map({
-      container: mapContainer,
-      style:
-        "https://api.maptiler.com/maps/streets/style.json?key=MZEJTanw3WpxRvt7qDfo",
-      hash: true,
-    });
-    map.addControl(new ScaleControl({}));
-    map.addControl(new NavigationControl({}), "bottom-right");
-
-    map.on("load", () => {
-      loaded = true;
-      // Debugging
-      //window.map = map;
-      mapStore.set(map);
-    });
-
-    const resizeObserver = new ResizeObserver(() => {
-      map.resize();
-    });
-    resizeObserver.observe(mapContainer);
-  });
-
-  onDestroy(() => {
-    map.remove();
-  });
+  $: if (loaded) {
+    mapStore.set(map);
+  }
 </script>
 
-<div class="map" bind:this={mapContainer}>
-  {#if loaded}
-    <slot />
-  {/if}
+<div>
+  <MapLibre
+    style="https://api.maptiler.com/maps/streets/style.json?key=MZEJTanw3WpxRvt7qDfo"
+    hash
+    bind:map
+    bind:loaded
+    on:error={(e) => {
+      // @ts-expect-error Not exported
+      console.log(e.detail.error);
+    }}
+  >
+    {#if $mapStore}
+      <ScaleControl />
+      <NavigationControl position="bottom-right" visualizePitch />
+      <slot />
+    {/if}
+  </MapLibre>
 </div>
 
 <style>
-  .map {
+  div {
+    position: relative;
     height: 100vh;
   }
 </style>
