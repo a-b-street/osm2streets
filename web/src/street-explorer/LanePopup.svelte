1a77f849f03d96d69f51f2d261e80b0fffc97719
Dustin Carlino <dabreegster@gmail.com>
2024-03-15 12:45:54 +0000

    Recalculate popup contents correctly

--- web/src/street-explorer/LanePopup.svelte
+++ web/src/street-explorer/LanePopup.svelte
@@ -24,45 +24,43 @@
   }
 </script>
 
-<div style="max-height: 50vh; max-width: 30vw;">
-  <h2>Lane {props.index} of Road {props.road}</h2>
-  <p><u>Type</u>: {props.type}</p>
-  <p><u>Direction</u>: {props.direction}</p>
-  <p><u>Width</u>: {props.width}m</p>
-  <p><u>Speed limit</u>: {props.speed_limit}</p>
-  <p><u>Allowed turns</u>: {props.allowed_turns}</p>
-  <p><u>Layer</u>: {props.layer}</p>
+<h2>Lane {props.index} of Road {props.road}</h2>
+<p><u>Type</u>: {props.type}</p>
+<p><u>Direction</u>: {props.direction}</p>
+<p><u>Width</u>: {props.width}m</p>
+<p><u>Speed limit</u>: {props.speed_limit}</p>
+<p><u>Allowed turns</u>: {props.allowed_turns}</p>
+<p><u>Layer</u>: {props.layer}</p>
 
-  {#if props.muv}
-    <details>
-      <summary>Full Muv JSON</summary>
-      <pre>{JSON.stringify(JSON.parse(props.muv), null, "  ")}</pre>
-    </details>
-  {/if}
+{#if props.muv}
+  <details>
+    <summary>Full Muv JSON</summary>
+    <pre>{JSON.stringify(JSON.parse(props.muv), null, "  ")}</pre>
+  </details>
+{/if}
 
-  <hr />
+<hr />
 
-  <p><u>OSM ways:</u></p>
-  {#each JSON.parse(props.osm_way_ids) as id}
-    <p>
-      <a href="https://www.openstreetmap.org/way/{id}" target="_blank">{id}</a>
-    </p>
-    <details>
-      <summary>See OSM tags</summary>
-      <table>
-        <tbody>
-          {#each Object.entries(JSON.parse(networkValue.getOsmTagsForWay(BigInt(id)))) as [key, value]}
-            <tr><td>{key}</td><td>{value}</td></tr>
-          {/each}
-        </tbody>
-      </table>
-    </details>
-  {/each}
+<p><u>OSM ways:</u></p>
+{#each JSON.parse(props.osm_way_ids) as id}
+  <p>
+    <a href="https://www.openstreetmap.org/way/{id}" target="_blank">{id}</a>
+  </p>
+  <details>
+    <summary>See OSM tags</summary>
+    <table>
+      <tbody>
+        {#each Object.entries(JSON.parse(networkValue.getOsmTagsForWay(BigInt(id)))) as [key, value]}
+          <tr><td>{key}</td><td>{value}</td></tr>
+        {/each}
+      </tbody>
+    </table>
+  </details>
+{/each}
 
-  <div>
-    <button type="button" on:click={collapse}>Collapse short road</button>
-    <button type="button" on:click={zip}>Zip side-path</button>
-  </div>
+<div>
+  <button type="button" on:click={collapse}>Collapse short road</button>
+  <button type="button" on:click={zip}>Zip side-path</button>
 </div>
 
 <style>
