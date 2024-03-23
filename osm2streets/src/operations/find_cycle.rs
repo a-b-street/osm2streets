4e6363a9472c929cd9799a9988ccd4c9633fe191
Dustin Carlino <dabreegster@gmail.com>
2024-03-23 13:35:12 +0000

    try DFS. we just need any cycle, really
    eh, it finds a cycle, but has to backtrack and so includes too much
    stuff

--- osm2streets/src/operations/find_cycle.rs
+++ osm2streets/src/operations/find_cycle.rs
@@ -13,53 +13,27 @@ enum Step {
 
 impl StreetNetwork {
     pub fn find_cycle(&self, start: IntersectionID) -> Result<String> {
-        let mut backrefs: HashMap<IntersectionID, RoadID> = HashMap::new();
-        // Never cross the same road twice
-        let mut visited: HashSet<RoadID> = HashSet::new();
-        // This is a max-heap, so negate all distances. Tie breaker is arbitrary but deterministic.
-        // Track where we're going and how we got there.
-        let mut queue: BinaryHeap<(Distance, IntersectionID, Option<RoadID>)> = BinaryHeap::new();
-        queue.push((Distance::ZERO, start, None));
+        // TODO Or even simpler... always try to go CW or CCW consistently. we dont want to
+        // backtrack ever.
 
-        while !queue.is_empty() {
-            let (dist_so_far, current, via_road) = queue.pop().unwrap();
-            if let Some(via) = via_road {
-                if visited.contains(&via) {
-                    continue;
+        let mut stack = vec![start];
+        let mut steps = Vec::new();
+        let mut visited = HashSet::new();
+
+        while let Some(current) = stack.pop() {
+            if visited.contains(&current) {
+                if current == start && steps.len() > 1 {
+                    return render(self, steps);
                 }
-                visited.insert(via);
-                backrefs.insert(current, via);
-            }
-            info!("Current step: {current} with dist {dist_so_far} via {:?}", via_road);
 
-            if current == start && dist_so_far != Distance::ZERO {
-                info!("  found cycle");
-                let mut steps = vec![Step::Node(current)];
-                let mut current = current;
-                loop {
-                    if current == start && steps.len() > 1 {
-                        /*steps.pop();
-                        steps.reverse();*/
-                        return render(self, steps);
-                    }
-                    let road = backrefs[&current];
-                    current = self.roads[&road].other_side(current);
-                    steps.push(Step::Edge(road));
-                    steps.push(Step::Node(current));
-                }
+                continue;
             }
+            visited.insert(current);
+            steps.push(Step::Node(current));
 
-            // when on i12, we skip over going to i17. when does it wind up in backrefs?
             for road in &self.intersections[&current].roads {
                 let next_i = self.roads[road].other_side(current);
-                if let Entry::Vacant(e) = backrefs.entry(next_i) {
-                    // DONT do this yet
-                    //e.insert(*road);
-                    // Remember to keep things negative
-                    let dist = dist_so_far - self.roads[road].center_line.length();
-                    queue.push((dist, next_i, Some(*road)));
-                    info!("  Havent been to {next_i} yet, so go there via {road}. dist will be {dist}");
-                }
+                stack.push(next_i);
             }
         }
 
